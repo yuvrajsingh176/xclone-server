1d50c85d3f761e2a1e0c1a01a99dcedaf7a6c443
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-02-20 12:09:06 +0530

    first commit

---
+++ src/app/index.ts
@@ -0,0 +1,32 @@
+import { ApolloServer } from "@apollo/server";
+import express from "express";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+import { prismaClient } from "../clients/db";
+import { User } from "./user";
+import cors from "cors";
+export async function initServer() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  const graphqlServer = new ApolloServer({
+    typeDefs: `
+    ${User.types}
+
+    type Query{
+${User.queries}
+    }
+  
+      `,
+    resolvers: {
+      Query: {
+        ...User.resolvers.queries,
+      },
+    },
+  });
+  await graphqlServer.start();
+
+  app.use("/graphql", expressMiddleware(graphqlServer));
+  return app;
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { initServer } from "./app";
+async function init() {
+  const app = await initServer();
+  app.listen(8000, () => console.log("hello"));
+}
+init();
